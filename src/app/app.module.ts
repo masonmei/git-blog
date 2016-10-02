cceec1b9f49755c8995c0cf8a6adb455c7fbe6aa
Mason <mason@igitras.com>
2016-10-03 01:29:04 +0800

    update version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
-import './rxjs-extension';
-
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-import { PersonListComponent } from './component/person-list/person-list.component';
-import { PersonDetailsComponent } from './component/person-details/person-details.component';
-
-import { routing } from './app-routing.module';
+import "./rxjs-extension";
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {PersonListComponent} from "./component/person-list/person-list.component";
+import {PersonDetailsComponent} from "./component/person-details/person-details.component";
+import {routing} from "./app-routing.module";
 
 @NgModule({
   declarations: [
